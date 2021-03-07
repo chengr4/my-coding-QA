b13918262e2cdc64933575e6a847e4faa14485f3
Eddie Cheng <54136390+chengr4@users.noreply.github.com>
2021-03-07 19:43:07 +0800

    update leetcode 1684

--- leetcode1684.py
+++ leetcode1684.py
@@ -32,17 +32,16 @@ class Solution:
         return ans
 
 # better answer
-# use set (集合 (Set) 其實和數組 (Tuple) 與串列 (List) 很類似, 不同的點在於集合不會包含重複的資料)
+# 使用 all(): 如果容器所有元素的值為True，那麼all函數的值才為True。注意若參數本身為空列表，all的值則為True
 
-# Runtime: 256 ms, faster than 48.21% of Python3 online submissions for Count the Number of Consistent Strings.
-# Memory Usage: 16.3 MB
+# Runtime: 260 ms, faster than 45% of Python3 online submissions for Count the Number of Consistent Strings.
+# Memory Usage: 16.1 MB
 
 class Solution2:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
         count = 0
-        sallowed = set(allowed)
-        
-        for i in words:
-            if all(j in sallowed for j in i):
+
+        for word in words:
+            if all(j in allowed for j in word):
                 count+=1
         return count
